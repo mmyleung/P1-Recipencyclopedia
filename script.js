15f7d91144c5a3368bccda2b1716f8776b789fc8
mmyleung <minamleung@gmail.com>
2023-05-26 13:15:30 +0100

    ajax call to openweathermap API to retrieve data from city entered

--- script.js
+++ script.js
@@ -2,4 +2,16 @@
 $("#location-form").on("submit", function(event) {
     event.preventDefault();
     console.log("submitted");   
+    //take value from inside input field and store in variable
+    var location = $("#location-input").val();
+    console.log(location);
+    //take location and pass it through openweathermap API
+    var APIKey = "0fdfab0bcf3590a8e7a2c9aecb8d3388";
+    var queryUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&appid=" + APIKey;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
 })

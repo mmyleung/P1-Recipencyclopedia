7d6502ebec086a06d53ee592e723b1dcfd0fa685
mmyleung <minamleung@gmail.com>
2023-05-26 13:07:49 +0100

    add event listener to location form

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //target form for location submission and set an event listener for submit
+$("#location-form").on("submit", function(event) {
+    event.preventDefault();
+    console.log("submitted");   
+})
